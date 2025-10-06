ba8f8e1fc82c0216d61b1d4901902fcd06528a38
Idkwhoami42 <neellodha42@gmail.com>
2025-10-06 19:27:57 +0200

    fix vite config and building

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { intlayerPlugin } from "vite-intlayer";
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
+  base: "./",
   plugins: [react(), tailwindcss(), intlayerPlugin()],
   resolve: {
     alias: {
