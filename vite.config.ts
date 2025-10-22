73a3843fb267d874b58d5e4319f7ce6066816d69
Idkwhoami42 <neellodha42@gmail.com>
2025-10-22 17:36:14 +0200

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { intlayerPlugin } from "vite-intlayer";
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
-  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : './',
+  base: './',
   plugins: [react(), tailwindcss(), intlayerPlugin()],
   resolve: {
     alias: {
